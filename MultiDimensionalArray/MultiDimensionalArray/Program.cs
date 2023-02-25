ac0770e69a5eb5f694e24836325ae109521aa6ba
Birodalmix <gudmann.benjamin@gmail.com>
2023-02-25 17:03:17 +0100

    Nested loops

--- MultiDimensionalArray/MultiDimensionalArray/Program.cs
+++ MultiDimensionalArray/MultiDimensionalArray/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,8 +10,50 @@ namespace MultiDimensionalArray
     internal class Program
     {
         static void Main(string[] args)
-        {
-            string[,] matrix; 
+        {   //declare 2D array 
+            string[,] matrix;
+            //3D array
+            int[,,] threeD;
+            // two dimensional array
+            int[,] array2D = new int[,]
+                {
+                    {1,2,3},// row 0
+                    {4,5,6},// row 1
+                    {7,8,9} // row 2
+                };
+            Console.WriteLine("Central value is {0}", array2D[1, 1]);
+            Console.ReadKey();
+
+            string[,,] array3D = new string[,,]
+                {
+                    {
+                        {"000","001","002"},
+                        {"010","011","012"}
+                    },
+                    {
+                        {"100","101","102"},
+                        {"110","111","112"}
+                    }
+                };
+            Console.WriteLine("Central value is {0}", array3D[0, 1, 1]);
+            Console.ReadKey();
+
+            string[,] array2dString = new string[3, 2]
+                {
+                    {"one","two"},
+                    {"three","four"},
+                    { "five","six"}
+                };
+            array2dString[1, 1]="chicken";
+            Console.WriteLine("The Value is {0}",array2dString[1,1]);
+            Console.ReadKey();
+
+            //Get back the amount of dimensions
+            int dimensions = array2dString.Rank;
+            Console.WriteLine(dimensions);
+            Console.ReadKey();
+            //Another type of declaration
+            int[,] array2dr = { {1,2},{1,2} };
         }
     }
 }
---
+++ Nested/Nested/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nested
+{
+    internal class Program
+    {
+        static int[,] matrix =
+        {
+            {0,1,2},
+            {3,4,5},
+            {6,7,8},
+        };
+        static void Main(string[] args)
+        {
+            //Printing all item in the matrix with foreach
+            foreach (var item in matrix)
+            {
+                Console.Write(item+" ");
+                Console.WriteLine("\n");
+            }
+            //Printing all item with nested for loop
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j]%2==0)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    } 
+                }
+            }
+        }
+    }
+}
