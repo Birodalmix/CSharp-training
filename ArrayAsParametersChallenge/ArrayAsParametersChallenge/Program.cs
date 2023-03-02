030a39c0a8a4a610b45b46ccb5b3248b19e02a7d
Birodalmix <gudmann.benjamin@gmail.com>
2023-03-02 08:38:01 +0100

    ArrayAsPararmeterChallenge

---
+++ ArrayAsParametersChallenge/ArrayAsParametersChallenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAsParametersChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] happiness = { 5, 12, 10, 8, 4 };
+            SunIsShining(happiness);
+            foreach (var item in happiness)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.ReadKey();
+        }
+        static void SunIsShining(int[] increaseble)
+        {
+            for (int i = 0; i < increaseble.Length; i++)
+            {
+                increaseble[i] += 2;
+            }
+        }
+    }
+}
